5d97d21fcdfbc09ba599e42003fe44e9c3c60fc7
ProFIT <santiago.farias@yandex.ru>
2021-10-21 17:55:12 +0700

    add get_coockies function

---
+++ main.py
@@ -0,0 +1,71 @@
+import pickle
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+import time
+
+from selenium.webdriver.common.by import By
+
+
+def get_cookies(driver):
+    """Получает куки от сайта и устанавливает их в драйвер"""
+    driver.get(url + '40482596')
+    time.sleep(3)
+
+    #Одобряем куки
+    driver.find_element(By.XPATH, '/html/body/div[8]/div/div[2]/button/span').click()
+
+    #Нажимаем на ссылку, чтобы узнать о наличии
+    link = driver.find_element(By.CLASS_NAME, 'range-revamp-stockcheck__available-for-delivery-link')
+    actions = ActionChains(driver)
+    actions.move_to_element(link).perform()
+    link.click()
+    time.sleep(1)
+
+    #Выбираем магазин
+    driver.find_element(By.XPATH, '/html/body/main/div/div[2]/div/div[4]/div/div[2]/div/div/div/div[4]/div[7]/div[2]/label/div/input').click()
+    pickle.dump(driver.get_cookies(), open(f"cookies", "wb"))
+    time.sleep(1)
+
+    #Одобряем выбор магазина
+    driver.find_element(By.XPATH, '/html/body/main/div/div[2]/div/div[4]/div/div[3]/button/span').click()
+    time.sleep(1)
+
+
+# options
+options = webdriver.ChromeOptions()
+
+# user-agent
+options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
+
+# for ChromeDriver version 79.0.3945.16 or over
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+options.add_argument("--no-sandbox")
+
+# headless mode
+# options.add_argument("--headless")
+# options.headless = True
+
+service = Service('/home/hozyain/parser/sonya/chromedriver')
+driver = webdriver.Chrome(
+    service=service,
+    options=options
+)
+
+url = 'https://www.ikea.com/ru/ru/p/-'
+
+try:
+
+    get_cookies(driver)
+    driver.get(url + '90483881')
+    time.sleep(5)
+
+
+
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
